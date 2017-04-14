e9c92c922d5309c26094c80a41bee0b530ac80f3
Bahareh SadeghianBoroujeni <sadeghib@student.tut.fi>
2017-04-14 12:57:00 +0300

    two way binding form

--- app.js
+++ app.js
@@ -5,13 +5,32 @@
     this.products = gems;
   });
 
+  app.controller('PanelController', function(){
+    this.tab = 1;
+    this.selectTab = function (setTab)
+    {
+      this.tab = setTab;
+    };
+    this.isSelected = function(checkTab)
+    {
+      return this.tab === checkTab;
+    };
+  });
+
   var gems = [
-    { name: 'Azurite', price: 2.95 , images : [
+    { name: 'Azurite', price: 2.95 ,
+     images : [{full: 'gem.png'}] ,
+     description : '...',
+     reviews : [{ stars: 5, body:'I love this gem', author:'sb@st.ghg.com' }, {stars:2, body: 'it sucks', author: 'me@man.kjk.com'}]
+   },
+    { name: 'Bloodstone',
+     price: 5.95 ,
+     images:[
       {full: 'gem.png'}
-    ] , description : '...'},
-    { name: 'Bloodstone', price: 5.95 , images:[
-      {full: 'gem.png'}
-    ] , description : '...'},
+    ] ,
+    description : '...',
+    reviews : [{ stars: 4, body:'I love this shit', author:'sb@st.ghg.com' }, {stars:1, body: 'its sucks', author: 'me@man.kjk.com'}]
+  },
     { name: 'Zircon', price: 3.95 , images:[
       {full: 'gem.png'}
     ] , description : '...'}
